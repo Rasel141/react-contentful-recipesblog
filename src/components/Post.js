64d152920aaf5d1760aad93817c5d49c012e2257
Rasel <rrasel141@gmail.com>
2020-09-23 01:24:15 +0600

    create - react contentful recipesblog

---
+++ src/components/Post.js
@@ -0,0 +1,26 @@
+import marked from "marked";
+import React from "react";
+const Post = ({ article }) => {
+  console.log(article);
+
+  const { name, featuredImage, description } = article.fields;
+
+  const postDescription = marked(description);
+
+  return (
+    <div className="post">
+      <h2 className="title">{name}</h2>
+      {featuredImage && (
+        <img
+          className="featuredImage"
+          src={featuredImage.fields.file.url}
+          alt={name}
+          title={name}
+        />
+      )}
+      <section dangerouslySetInnerHTML={{ __html: postDescription }} />
+    </div>
+  );
+};
+
+export default Post;
